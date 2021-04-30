6e7323a51d45d06488e068bd6460764057bd3e31
agouy <contact@agouy.com>
2021-04-30 14:04:52 +0000

    matrix issue

--- main.R
+++ main.R
@@ -5,7 +5,9 @@ library(reticulate)
 reticulate::source_python("main.py")
 
 df <- (ctx <- tercenCtx()) %>%
-  as.matrix(fill=0) %>% run_py_script()
+  as.matrix(fill=0) %>%
+  t() %>%
+  run_py_script()
 
 colnames(df) <- c("trimap1", "trimap2")
 df <- as.data.frame(df)
